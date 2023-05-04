0ee69344d92cb3a13c617cd79a3c301f6e3100f6
thiago-tsms <thiago-tsms@hotmail.com>
2023-05-03 23:36:58 -0300

    modified:   "Laborat\303\263rio III/mine_cliente.py" 	modified:   "Laborat\303\263rio III/mine_server.py"

--- Laboratório III/mine_cliente.py
+++ Laboratório III/mine_cliente.py
@@ -4,66 +4,137 @@ import mine_grpc_pb2_grpc
 
 import tkinter as tk
 
+client_id = 0
+transaction_id = 0
+challenge = 0
+status = 0
+seed = 0
 
-def getTransactionID(client):
+def leitura(input):
+    try:
+        id = int(input.get())
+        return id
+    except:
+        print('É necessário inserir um valor.')
+        return -1
+    finally:
+        input.delete(0, tk.END)
+
+
+def getTransactionID(client, input):
     try:
         res = client.getTransactionId(mine_grpc_pb2.void())
-        print(f'ID da transação: {res.result}')
+        transaction_id = res.result
+        print(f'ID da transação: {transaction_id}')
     except:
         print(f'Erro: getTransactionID')
+
+
+def getChallenge(client, input):
     
-def getChallenge(client):
-    res = client.getChallenge(mine_grpc_pb2.transactionId())
-    print(f'Resultado: {res.result}')
+    id = leitura(input)
+    if(id == -1):
+        return
 
-def getTransactionStatus(client):
-    res = client.getTransactionStatus(mine_grpc_pb2.transactionId())
-    print(f'Resultado: {res.result}')
+    try:
+        res = client.getChallenge(mine_grpc_pb2.transactionId(transactionId=id))
+        challenge = res.result
+        print(f'Valor do desafio: {challenge}')
+    except:
+        print(f'Erro: getChallenge')
 
-def getWinner(client):
-    res = client.getWinner(mine_grpc_pb2.transactionId())
-    print(f'Resultado: {res.result}')
 
-def GetSolucion(client):
-    res = client.GetSolucion(mine_grpc_pb2.transactionId())
-    print(f'Resultado: {res.result}')
+def getTransactionStatus(client, input):
 
-def getSolucion(client):
-    res = client.getSolucion(mine_grpc_pb2.transactionId())
+    id = leitura(input)
+    if(id == -1):
+        return
     
-    status = res.status
-    result = res.result
-    challenge = res.challenge
-    
-    print(f'Resultado: {status} - {result} - {challenge}')
+    try:
+        res = client.getTransactionStatus(mine_grpc_pb2.transactionId(transactionId=id))
+        status = res.result
+        print(f'Status da Transação: {status}')
+    except:
+        print(f'Erro: getTransactionStatus')
 
-def mine(client):
-    #res = client.add(mine_grpc_pb2.GetSolucion())
-    print(f'Resultado: ')
+
+def submitChallenge(client, input):
+    try:
+        res = client.submitChallenge(mine_grpc_pb2.challengeArgs(transactionId=transaction_id, clientId=client_id, seed=seed))
+        print(f'Validade da solução: {res.result}')
+    except:
+        print(f'Erro: submitChallenge')
+
+
+def getWinner(client, input):
+    id = leitura(input)
+    if(id == -1):
+        return
+    
+    try:
+        res = client.getWinner(mine_grpc_pb2.transactionId(transactionId=id))
+        print(f'Id do vencedor: {res.result}')
+    except:
+        print(f'Erro: getWinner')
+
+
+def getSolution(client, input):
+    id = leitura(input)
+    if(id == -1):
+        return
+
+    # try:
+    #     res = client.getSolucion(mine_grpc_pb2.transactionId(transactionId=id))
+    #     print(f'Status: {res.status} \nDesafio: {res.challenge} \n Resultado: {res.result}')
+    # except:
+    #     print(f'Erro: getSolucion')
+
+    res = client.getSolution(mine_grpc_pb2.transactionId(transactionId=id))
+    print(f'Status: {res.status} \nDesafio: {res.challenge} \n Resultado: {res.result}')
+
+
+def mine(client, input):
+    try:
+        id = client.getTransactionId(mine_grpc_pb2.void()).result
+        challenge = client.getChallenge(mine_grpc_pb2.transactionId(transactionId=id)).result
+        status = client.getTransactionStatus(mine_grpc_pb2.transactionId(transactionId=id)).result
+        
+        print(f'ID transação: {id} \nDesafio: {challenge} \nStatus: {status}')
+        
+        #Busca a sulução
+        #Imprime a solução
+        result = client.submitChallenge(mine_grpc_pb2.challengeArgs(transactionId=id, clientId=client_id, seed=seed))
+        #imprime result
+    except:
+        print(f'Erro: mine')
 
 
 def run():
     channel = grpc.insecure_channel('localhost:8080')
     client = mine_grpc_pb2_grpc.apiStub(channel)
-    
-    opcoes = [
-        ("getTransactionID", lambda: getTransactionID(client)),
-        ("getChallenge", lambda: getChallenge(client)),
-        ("getTransactionStatus", lambda: getTransactionStatus(client)),
-        ("getWinner", lambda: getWinner(client)),
-        ("GetSolucion", lambda: getSolucion(client)),
-        ("Mine", lambda: mine(client)),
-    ]
 
     page = tk.Tk()
+    input = tk.Entry(page, justify=tk.CENTER, borderwidth=3, font=12, width=30)
+
+    opcoes = [
+        ("getTransactionID", lambda: getTransactionID(client, input)),
+        ("getChallenge", lambda: getChallenge(client, input)),
+        ("getTransactionStatus", lambda: getTransactionStatus(client, input)),
+        ("submitChallenge", lambda: submitChallenge(client, input)),
+        ("getWinner", lambda: getWinner(client, input)),
+        ("getSolution", lambda: getSolution(client, input)),
+        ("mine", lambda: mine(client, input)),
+    ]
+    
     page.title('gRPC - python')
 
     tk.Label(page)
     tk.Label(page, text="Selecione uma das opções", justify=tk.LEFT, padx=10, pady=10, font=15).pack()
     [tk.Button(page, text=txt, command=func, width=50, pady=5, padx=15, justify=tk.CENTER).pack(anchor=tk.W) for (txt, func) in opcoes]
+
+    input.pack(padx=10, pady=10)
     
     page.mainloop()
-
 
 if __name__ == '__main__':
     run()
--- Laboratório III/mine_server.py
+++ Laboratório III/mine_server.py
@@ -2,55 +2,74 @@ import grpc
 import mine_grpc_pb2
 import mine_grpc_pb2_grpc
 from concurrent import futures
+import random
 
 class ApiServicer(mine_grpc_pb2_grpc.apiServicer):
-    '''transaction_id = 0
-    challenge = 0
+    transaction_id = 0
+    challenge = random.randint(0, 7)
     solution = ''
-    winner = -1'''
+    winner = -1
+    status = False
 
     def getTransactionId(self, request, context):
-        #print(f'Executando: getTransactionId')
-        
-        return mine_grpc_pb2.intResult(result=0)
+        return mine_grpc_pb2.intResult(result=(self.transaction_id))
 
 
     def getChallenge(self, request, context):
-        transaction_id = request.result
-        
-        print(f'{transaction_id}')
+        transaction_id = request.transactionId
 
-        return mine_grpc_pb2.intResult(result=0)
+        if(self.transaction_id == transaction_id):
+            return mine_grpc_pb2.intResult(result=(self.challenge))
+        else:
+            return mine_grpc_pb2.intResult(result=(-1))
 
+       
     def getTransactionStatus(self, request, context):
         transaction_id = request.transactionId
-        
-        print(f'{transaction_id}')
-        
-        return mine_grpc_pb2.intResult(result=0)
+
+        if(self.transaction_id != transaction_id):
+            return mine_grpc_pb2.intResult(result=-1)
+        elif(self.status == False):
+            return mine_grpc_pb2.intResult(result=0)
+        elif(self.status == True):
+            return mine_grpc_pb2.intResult(result=1)
+
 
     def submitChallenge(self, request, context):
-        transaction_id = request.result
+        transaction_id = request.transactionId
         client_id = request.clientId
         soluction = request.seed
-        
-        print(f'{transaction_id} - {client_id} - {soluction}')
 
-        return mine_grpc_pb2.intResult(result=0)
+        if(self.transaction_id != transaction_id):
+            return mine_grpc_pb2.intResult(result=(-1))
+        else:
+            print(f'Efetua o teste de verificação.')
+            return mine_grpc_pb2.intResult(result=0)
+        
+            # 1 OK
+            # 0 Inválida
+            # 2 Desafio solucionado
+
 
     def getWinner(self, request, context):
-        transaction_id = request.result
+        transaction_id = request.transactionId
+
+        if(self.transaction_id != transaction_id):
+            return mine_grpc_pb2.intResult(result=-1)
+        elif(self.solution == ''):
+            return mine_grpc_pb2.intResult(result=0)
+        else:
+            return mine_grpc_pb2.intResult(result=self.winner)
         
-        print(f'{transaction_id}')
-
-        return mine_grpc_pb2.intResult(result=0)
-
+   
     def getSolution(self, request, context):
-        transaction_id = request.result
-        
-        print(f'{transaction_id}')
+        transaction_id = request.transactionId
 
-        return mine_grpc_pb2.structResult(status=1, result=1, challenge=1)
+        if(self.transaction_id != transaction_id):
+            return mine_grpc_pb2.structResult(status=-1, result='', challenge=-1)
+        else:
+            return mine_grpc_pb2.structResult(status=self.status, result=self.solution, challenge=self.challenge)
+
 
 def run():
     grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
