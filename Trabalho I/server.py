2045d2de042bb8ee69c93c2ca32f364bb6efa86e
thiago-tsms <thiago-tsms@hotmail.com>
2023-05-21 18:41:45 -0300

    deleted:    Trabalho I/client.py 	renamed:    Trabalho I/teste_cliente.py -> Trabalho I/cliente.py 	modified:   Trabalho I/server.py 	deleted:    Trabalho I/teste_server.py

--- Trabalho I/server.py
+++ Trabalho I/server.py
@@ -1,68 +1,65 @@
-import paho.mqtt.client as mqtt 
-import flwr as fl
-#from random import randrange, uniform
+from mqtt import ComunicacaoMQTTServer
+from aprendizado import *
 import time
-import sys
 
-idClient = ""
-
-def on_message_idclient(client, userdata, message):
-    global idClient
-    idClient = str(message.payload.decode("utf-8"))
-
-
-def weighted_average(metrics):
-    # Multiply accuracy of each client by number of examples used
-    acc = [num_examples * m["accuracy"] for num_examples, m in metrics]
-    examples = [num_examples for num_examples, _ in metrics]
-
-    # Aggregate and return custom metric (weighted average)
-    results = {"accuracy": sum(acc) / sum(examples)}
-    return results
-
-strategy = fl.server.strategy.FedAvg(
-    fraction_fit=0.9,           # Escolher aleatoriamento 90% dos clientes
-    fraction_evaluate=1,        # Usar todos os clientes para avaliação do modelo
-    min_fit_clients=5,          # Minimo de 5 clientes para o treinamento
-    min_evaluate_clients=5,    # Mínimo de 5 clientes para avaliação
-    min_available_clients=5,    # Mínimo de 5 clientes para comessar o treinamento
-    evaluate_metrics_aggregation_fn=weighted_average,   # Usar esta função como função de agregação da métricas
-)
-
-
-
-# Calcula total de argumentos
-n = len(sys.argv)
- 
-mqttBroker = "broker.emqx.io" 
-if len(sys.argv) >= 2:
-    mqttBroker = str(sys.argv[1])
-
-print(mqttBroker)
 
 #Parametros de inicialização
 nClients = 3
-nMinClients = 5
-nMaxRouds = 10
-MetaAcuracia = 100
-TimeOut = 1 #Não entendi bem este aqui
+nMinClients = 2
+nMaxRouds = 4
+#MetaAcuracia = 100
+TimeOut = 5
 
-#Filas
-fila_idClient = "sd/id/client"
+input_shape = (28, 28, 1)
+num_classes = 10
+num_clients = 10
 
-client = mqtt.Client("Server_Federado_1")
-client.connect(mqttBroker) 
 
-while True:
-    client.loop_start()
-    client.subscribe(fila_idClient)
-    client.on_message=on_message_idclient
-    time.sleep(1)
-    client.loop_stop()
-    print("ID Client: " + idClient)
-    idClient = ""
+def aguarda_condicoes_iniciais(mqtt):
+    print(f'Aguardando número de clientes')
+    while len(mqtt.get_clientes()) < nMinClients:
+        time.sleep(1)
+    print(f'Aprendizado federado iniciando')
     
-#    randNumber = uniform(0, 100)
+
+def convert(res, lista):
+    for i in lista:
+        return res.append
+
+def run():
+    mqtt = ComunicacaoMQTTServer(TimeOut)
     
-#    client.publish(topic, randNumber)
-    time.sleep(1)
+    # Espera o número mínimo de clientes para comessar a execução
+    #aguarda_condicoes_iniciais(mqtt)
+    
+    (x_train, y_train, x_test, y_test) = obtem_dados(num_clients)
+    aprendizado = Aprendizado(x_train, y_train, x_test, y_test, input_shape, num_classes)
+    
+    #res = aprendizado.get_weights()
+    
+    model_weights = aprendizado.get_weights()
+
+    for _ in range(nMaxRouds):
+        all_weights = []
+                
+        # Espera ter o número mínimo de clientes para comessar a execução
+        aguarda_condicoes_iniciais(mqtt)
+    
+        # Tenta enviar os gradientes // retorna para quantos estão ouvindo
+        n_clientes = mqtt.start_aprendizado(model_weights)
+        
+        # Espera a recepção de todos
+        for _ in range(n_clientes):
+            all_weights.append(aprendizado.re_shape(mqtt.get_gradientes()))
+        
+        model_weights = aprendizado.federated_averaging(model_weights, all_weights)
+        
+        print(aprendizado.evaluate(model_weights))
+
+    mqtt.finalizar_mqtt()
+    
+    print('Aprendizado finalizado')
+    
+    
+if __name__ == '__main__':
+    run()   
