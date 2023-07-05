f7fbc116a1ebbcb3ca798525892bad3efe236d5b
thiago-tsms <thiago-tsms@hotmail.com>
2023-07-05 13:32:57 -0300

    modified:   "Laborat\303\263rio VII/crypto.py" 	modified:   "Laborat\303\263rio VII/main.py"

--- Laboratório VII/crypto.py
+++ Laboratório VII/crypto.py
@@ -2,8 +2,15 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-
 from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidSignature
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.x509 import CertificateBuilder
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+from cryptography.x509.oid import NameOID
+import datetime
+
 
 class Security():
 
@@ -53,7 +60,7 @@ class Security():
         ).hex()
 
 
-    # Descriptografa usando cha privada local
+    # Descriptografa usando chave privada local
     def descritografar(self, data):
         return self.__private_key.decrypt(
             bytes.fromhex(data),
@@ -64,3 +71,66 @@ class Security():
             )
         ).decode('utf-8')
 
+    # Efetua a assinatura de uma mensagem
+    def assinar_mensagem(self, data):
+        return self.__private_key.sign(
+            bytes(data, "utf-8"),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        ).hex()
+        
+    # Verifica a assinatura de uma mensagem
+    def verificar_assinatura(self, public_key, data, signature):
+        pk = serialization.load_pem_public_key(
+            bytes.fromhex(public_key),
+            backend=default_backend(),
+        )
+        try:
+            pk.verify(
+                bytes.fromhex(signature),
+                bytes(data, "utf-8"),
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            return True
+        except InvalidSignature:
+            return False
+    
+    # Gera um certificado digital
+    def gerar_certificado_digital(self, id):
+        subject = issuer = x509.Name([
+            x509.NameAttribute(NameOID.USER_ID, str(id)),
+            x509.NameAttribute(NameOID.COUNTRY_NAME, u"BR"),
+            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Espirito Santo"),
+            x509.NameAttribute(NameOID.LOCALITY_NAME, u"Vitoria"),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"Universidade Federal do Espirito Santo (Ufes)"),
+            x509.NameAttribute(NameOID.COMMON_NAME, u"UFES"),
+        ])
+
+        cert_builder = x509.CertificateBuilder()
+        cert_builder = cert_builder.subject_name(subject)
+        cert_builder = cert_builder.issuer_name(issuer)
+        cert_builder = cert_builder.public_key(self.__public_key)
+        cert_builder = cert_builder.serial_number(x509.random_serial_number())
+        cert_builder = cert_builder.not_valid_before(datetime.datetime.utcnow())
+        cert_builder = cert_builder.not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+        cert_builder = cert_builder.add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True)
+        cert_builder = cert_builder.sign(self.__private_key, hashes.SHA256(), default_backend())
+
+        return cert_builder.public_bytes(Encoding.PEM)
+    
+    # Verifica um certificado digital
+    def verificar_certificado_digital(self, certificate):
+        loaded_certificate = x509.load_pem_x509_certificate(certificate, default_backend())
+
+        current_time = datetime.datetime.utcnow()
+        if loaded_certificate.not_valid_before <= current_time <= loaded_certificate.not_valid_after:
+            return True
+        else:
+            return False
--- Laboratório VII/main.py
+++ Laboratório VII/main.py
@@ -12,6 +12,14 @@ def run():
     ciphertext = sec.criptografar(json.dumps(data), sec.public_key_hex)
     print(json.loads(sec.descritografar(ciphertext)))
 
+    signature = sec.assinar_mensagem(json.dumps(data))
+    status = sec.verificar_assinatura(sec.public_key_hex, json.dumps(data), signature)
+    print(status)
+
+    certificado = sec.gerar_certificado_digital(1)
+    sec.verificar_certificado_digital(certificado)
+    #print(certificado)
+
 
 if __name__ == '__main__':
     run()
